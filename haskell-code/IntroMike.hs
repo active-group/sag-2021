e2e943ccb38444544826a9040c79fc730bc67fda
Mike Sperber <sperber@deinprogramm.de>
2021-10-12 09:29:28 +0200

    Update.

--- haskell-code/IntroMike.hs
+++ haskell-code/IntroMike.hs
@@ -4,3 +4,11 @@ import Prelude hiding (Functor, Monad, Monoid, Semigroup)
 
 x :: Integer
 x = 12
+
+-- Animals on the Texas highway
+
+-- Armadillo has the following properties:
+-- - dead or alive
+-- - weight 
+
+data Liveness = Dead | Alive
